fed8a959baca44ea6b34f420a60f917d77a60c80
Mohammed Hashim <mohammed.hashim@coriolis.co.in>
2026-02-21 16:11:09 +0530

    adding ajman tax

--- greenwheels/hooks.py
+++ greenwheels/hooks.py
@@ -261,3 +261,4 @@ doc_events = {
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures = [{"doctype": "Account"}]
