091e6d99b300a67825b73c7482a142340a46aedf
Mohammed Hashim <mohammed.hashim@coriolis.co.in>
2026-02-21 16:38:14 +0530

    fixture  removed

--- greenwheels/hooks.py
+++ greenwheels/hooks.py
@@ -260,5 +260,3 @@ doc_events = {
 # ------------
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
-
-fixtures = [{"doctype": "Account"}]
